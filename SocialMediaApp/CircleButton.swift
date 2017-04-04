9d1e13b8f85efb8aa21f15622077af12249bf2e9
Milos Mladenovic <milosmladenovic@icloud.com>
2017-04-04 14:11:55 +0200

    Settiing up Facebook authentication

--- SocialMediaApp/CircleButton.swift
+++ SocialMediaApp/CircleButton.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2017 Milos Mladenovic. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+    
+    @IBInspectable var circleradius: CGFloat = 30.0 {
+        didSet{
+            setupView()
+        }
+    }
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    func setupView() {
+        layer.cornerRadius = circleradius
+    }
+}
--- SocialMediaApp/SignInVC.swift
+++ SocialMediaApp/SignInVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
@@ -19,7 +21,34 @@ class SignInVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func facebookButtonTapped(_ sender: Any) {
+        
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("User cancelled Facebook authentication")
+            } else {
+                print("JSuccessfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("Successfully authenticated with Firebase")
+            
+                }
+        })
+    }
 
 
 }
-
